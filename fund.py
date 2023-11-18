ec9c8697b39f1f3814352f0a9bd4ded872310116
Nathan Peirce <peirce.places@gmail.com>
2023-11-18 00:57:01 -0700

    Added more testing of the contributions classes

--- fund.py
+++ fund.py
@@ -1,6 +1,7 @@
 from enum import Enum, auto
 from abc import ABC, abstractmethod
-from datetime import datetime, timedelta
+from datetime import timedelta
+from datetime import date
 
 
 class Frequency(Enum):
@@ -10,13 +11,18 @@ class Frequency(Enum):
     ONCE = auto()
 
 
-class Contribution(ABC):
-    def __init__(self, amount: float, start_date: datetime):
+class BaseContributionSchedule(ABC):
+    def __init__(self, amount: float, reference_date: date):
+        """
+        :param amount: The amount that is regularly contributed
+        :param reference_date: The date that starts a periodic schedule (contributions don't start until subsequent \
+        periods AFTER this date)
+        """
         self.amount = amount
-        self.last_contribution_date = start_date
+        self.last_contribution_date = reference_date
 
     @abstractmethod
-    def contribute_until_date(self, date: datetime) -> float:
+    def contribute_until_date(self, date: date) -> float:
         """
         Advances last contribution date to last date of contribution before date
         and returns the amount contributed in that time
@@ -27,46 +33,92 @@ class Contribution(ABC):
         ...
 
 
-class MonthlyContribution(Contribution):
-    def contribute_until_date(self, date: datetime) -> float:
-        months = 12*(date.year - self.last_contribution_date.year) + (date.month - self.last_contribution_date.month)
-        self.last_contribution_date = datetime(self.last_contribution_date.year, self.last_contribution_date.month+months, self.last_contribution_date.day)
-        return self.amount * months
+class MonthlyContributions(BaseContributionSchedule):
+    def contribute_until_date(self, next_date: date) -> float:
+        months = 12 * (next_date.year - self.last_contribution_date.year) + (
+                    next_date.month - self.last_contribution_date.month)
+        elapsed_contributions = months
+        if next_date.day < self.last_contribution_date.day:
+            elapsed_contributions -= 1  # Pay day not reached for last month
+
+        self.last_contribution_date = \
+            date(self.last_contribution_date.year + elapsed_contributions // 12,
+                 self.last_contribution_date.month + elapsed_contributions % 12,
+                 self.last_contribution_date.day)
+        return self.amount * elapsed_contributions
 
 
-class BiweeklyContribution(Contribution):
-    def contribute_until_date(self, date: datetime) -> float:
-        two_week_periods = ((date - self.last_contribution_date).days//7)//2
-        self.last_contribution_date += timedelta(days=two_week_periods*14)
+class BiweeklyContributions(BaseContributionSchedule):
+    def contribute_until_date(self, next_date: date) -> float:
+        two_week_periods = ((next_date - self.last_contribution_date).days // 7) // 2
+        self.last_contribution_date += timedelta(days=two_week_periods * 14)
         return self.amount * two_week_periods
+
+
+class SemiMonthlyContributions(BaseContributionSchedule):
+    """
+    Semi-Monthly schedule on the 1st and 15th of the month
+    """
+
+    def __init__(self, amount: float, reference_date: date):
+        super(SemiMonthlyContributions, self).__init__(amount, reference_date)
+        self.last_contribution_date = self.get_nearest_1st_or_15th(self.last_contribution_date)
+
+    def contribute_until_date(self, next_date: date) -> float:
+        next_date = self.get_nearest_1st_or_15th(next_date)
+
+        months = 12 * (next_date.year - self.last_contribution_date.year) + (
+                    next_date.month - self.last_contribution_date.month)
+        elapsed_contributions = 0
+        if months >= 0:
+            if months == 0:
+                if next_date > self.last_contribution_date:
+                    elapsed_contributions = 1
+                else:
+                    elapsed_contributions = 0
+            else:
+                if next_date.day > self.last_contribution_date.day:
+                    elapsed_contributions = months + 1
+                else:
+                    elapsed_contributions = months
+        if elapsed_contributions:
+            self.last_contribution_date = next_date
+        return self.amount * elapsed_contributions
+
+    @staticmethod
+    def get_nearest_1st_or_15th(next_date):
+        if next_date.day < 15:
+            nearest_1st_or_15th = date(next_date.year, next_date.month, 1)
+        else:
+            nearest_1st_or_15th = date(next_date.year, next_date.month, 15)
+        return nearest_1st_or_15th
 
 
 class Fund:
     def __init__(self):
-        self.current_date = datetime.now()
+        self.current_date = date.today()
         self.balance = 0.0
-        self.contributions: list[Contribution] = []
+        self.contributions: list[BaseContributionSchedule] = []
 
-    def contribute(self, amount: float, date: datetime = None, *, frequency: Frequency = Frequency.ONCE) -> None:
-        if date is None:
-            date = datetime.now()
+    def contribute(self, amount: float, reference_date: date = None, *, frequency: Frequency = Frequency.ONCE) -> None:
+        if reference_date is None:
+            reference_date = date.today()
         self.current_date = date
         if frequency == Frequency.ONCE:
             self.balance += amount
         if frequency == Frequency.MONTHLY:
-            self.contributions.append(MonthlyContribution(amount, date))
+            self.contributions.append(MonthlyContributions(amount, reference_date))
         if frequency == Frequency.BIWEEKLY:
-            self.contributions.append(BiweeklyContribution(amount, date))
+            self.contributions.append(BiweeklyContributions(amount, reference_date))
 
     def get_balance(self) -> float:
         return self.balance
 
-    def advance_time(self, new_date: datetime = None):
+    def advance_time(self, new_date: date = None):
         if new_date is None:
-            new_date = datetime.now()
+            new_date = date.now()
 
         for contribution in self.contributions:
             self.balance += contribution.contribute_until_date(new_date)
 
         self.current_date = new_date
-
--- wealth_unit_test.py
+++ wealth_unit_test.py
@@ -1,5 +1,9 @@
 import unittest
-from datetime import datetime, timedelta
+from datetime import date, timedelta
+
+from typing import List
+
+import fund
 from fund import Frequency, Fund
 
 
@@ -10,21 +14,65 @@ class MyTestCase(unittest.TestCase):
     def test_can_instantiate_fund(self):
         self.assertIsNotNone(self.fund)
 
+
+    def test_contribution_schedules(self):
+        contribution_types: List[fund.BaseContributionSchedule]
+        totals = [int(0)] * 3
+        start_date = date(1900, 1, 4)
+        last_contribution_dates = [start_date] * 3
+
+        def advance_contributions_until(next_date: date):
+            for i, c in enumerate(contribution_types):
+                totals[i] += c.contribute_until_date(next_date)
+                last_contribution_dates[i] = c.last_contribution_date
+        cm = fund.MonthlyContributions(1, start_date)
+        cbw = fund.BiweeklyContributions(1, start_date)
+        csm = fund.SemiMonthlyContributions(1, start_date)
+        contribution_types = [cm, cbw, csm]
+
+        self.assertEqual([start_date, start_date, date(1900, 1, 1)], [cm.last_contribution_date, cbw.last_contribution_date, csm.last_contribution_date])
+        self.assertEqual([0, 0, 0], totals)  # Nothing contributed yet
+
+        advance_contributions_until(date(1900, 1, 14))  # A just before a semi-monthly
+        self.assertEqual([start_date, start_date, date(1900, 1, 1)], last_contribution_dates)
+        self.assertEqual([0, 0, 0], totals)  # Nothing contributed yet
+
+        advance_contributions_until(date(1900, 1, 15))  # Semi-monthly is the first to pay
+        self.assertEqual([start_date, start_date, date(1900, 1, 15)], last_contribution_dates)
+        self.assertEqual([0, 0, 1], totals)
+
+        advance_contributions_until(date(1900, 1, 18))  # First bi-weekly
+        self.assertEqual([start_date,  date(1900, 1, 18), date(1900, 1, 15)], last_contribution_dates)
+        self.assertEqual([0, 1, 1], totals)
+
+        advance_contributions_until(date(1900, 2, 1))  # Semi-monthly and Bi-weekly both hit on Feb 1
+        self.assertEqual([start_date,  date(1900, 2, 1), date(1900, 2, 1)], last_contribution_dates)
+        self.assertEqual([0, 2, 2], totals)
+
+        advance_contributions_until(date(1900, 2, 4))  # First monthly comes in
+        self.assertEqual([date(1900, 2, 4),  date(1900, 2, 1), date(1900, 2, 1)], last_contribution_dates)
+        self.assertEqual([1, 2, 2], totals)
+
+        advance_contributions_until(date(1901, 2, 4))  # 1 year later
+        self.assertEqual([date(1901, 2, 4),  date(1901, 2, 1), date(1901, 2, 1)], last_contribution_dates)
+        self.assertEqual([1 + 12, 2 + 52/2, 2 + 24], totals)
+
+
     def test_can_contribute_to_fund(self):
         self.fund.contribute(10_000.00)
         balance = self.fund.get_balance()
         self.assertEqual(balance, 10_000)
 
     def test_can_contribute_monthly(self):
-        start_day = datetime(2023, 10, 1)
-        self.fund.contribute(1000, date=start_day, frequency=Frequency.MONTHLY)
-        self.fund.advance_time(datetime(2023, 12, 1))
+        start_day = date(2023, 10, 1)
+        self.fund.contribute(1000, reference_date=start_day, frequency=Frequency.MONTHLY)
+        self.fund.advance_time(date(2023, 12, 1))
         balance = self.fund.get_balance()
         self.assertEqual(balance, 2000)
 
     def test_can_contribute_biweekly(self):
-        start_day = datetime(2023, 10, 1)
-        self.fund.contribute(1000, date=start_day, frequency=Frequency.BIWEEKLY)
+        start_day = date(2023, 10, 1)
+        self.fund.contribute(1000, reference_date=start_day, frequency=Frequency.BIWEEKLY)
         self.fund.advance_time(start_day + timedelta(days=6, weeks=3))
         balance = self.fund.get_balance()
         self.assertEqual(balance, 1000)
